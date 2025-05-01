673220cb6fbdb13958ba59dd69ec00add87bb9dc
Renato Coello <88224209+Renatocm0708@users.noreply.github.com>
2025-05-01 13:36:15 -0500

    Update AccessMap.tsx

--- src/components/AccessMap.tsx
+++ src/components/AccessMap.tsx
@@ -89,12 +89,12 @@ const AccessMap = () => {
           
           {/* Top label */}
           <div className="absolute top-0 left-1/2 -translate-x-1/2 bg-slate-300 px-4 py-0.5 rounded text-sm font-medium">
-            Entrada Principal
+            Almax +
           </div>
           
           {/* Bottom label */}
           <div className="absolute bottom-0 left-1/2 -translate-x-1/2 bg-slate-300 px-4 py-0.5 rounded text-sm font-medium">
-            Entrada Secundaria
+            Entrada Principal
           </div>
           
           {/* Left label */}
