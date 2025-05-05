2af59040d7154e510d80e574f13477b0914caa2f
Renato Coello <88224209+Renatocm0708@users.noreply.github.com>
2025-05-05 18:08:40 -0500

    Update VerticalSeparators.tsx

--- src/components/access-map/VerticalSeparators.tsx
+++ src/components/access-map/VerticalSeparators.tsx
@@ -3,11 +3,11 @@ const VerticalSeparators = () => {
   return (
     <>
       {/* Left side separators */}
-      <div className="absolute top-10 bottom-10 left-[190px] border-l-2 border-dashed border-slate-400"></div>
+      <div className="absolute top-10 bottom-10 left-[180px] border-l-2 border-dashed border-slate-400"></div>
       
       {/* Right side separators */}
       <div className="absolute top-10 bottom-10 right-[300px] border-l-2 border-dashed border-slate-400"></div>
-      <div className="absolute top-10 bottom-10 right-[200px] border-l-2 border-dashed border-slate-400"></div>
+      <div className="absolute top-10 bottom-10 right-[180px] border-l-2 border-dashed border-slate-400"></div>
     </>
   );
 };
